07c49dcc09a3b1342d9c9db5f14ea77f747b1367
hargathor <3949704+hargathor@users.noreply.github.com>
2017-11-29 11:37:35 +0100

    Adding postgis conf regarding the new map

--- postgis/initdb-postgis.sh
+++ postgis/initdb-postgis.sh
@@ -24,6 +24,7 @@ EOSQL
 done
 
 #import Melbourne city
-osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /melbourne_australia.osm.pbf
+#osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /melbourne_australia.osm.pbf
+osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /ile-de-france-latest.osm.pbf
 
 touch /var/lib/postgresql/data/DB_INITED
